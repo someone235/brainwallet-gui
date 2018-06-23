93464991a591188d5199bc64db5082cc9a92febf
Ori Newman <orinewman1@gmail.com>
2018-06-23 22:54:44 +0300

    electron builder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-window.openDevTools();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
